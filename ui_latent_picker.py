f86bb685e4dd5f66e18e6a614a285f2a1883c601
CodeHatchling <steve@codehatch.com>
2025-01-01 21:54:21 -0700

    Added pages and a fixed number of elements to the color swatches array.

--- ui_latent_picker.py
+++ ui_latent_picker.py
@@ -1,11 +1,11 @@
 from typing import Callable
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QMouseEvent, QPixmap
+from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import (
     QLabel,
     QWidget,
-    QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QFrame, QSizePolicy, QTabWidget, QBoxLayout
+    QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QFrame, QSizePolicy, QTabWidget
 )
 
 from ui_widgets import Slider
@@ -57,6 +57,8 @@ class ColorPickerSwatch(QPushButton):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.latent_value: tuple[float, ...] = (0, 0, 0, 0)
+
         self.setFixedWidth(40)
         self.setFixedHeight(40)
 
@@ -73,6 +75,127 @@ class ColorPickerSwatch(QPushButton):
         self._label.setPixmap(pixmap)
 
 
+class ColorPickerSwatches(QFrame):
+
+    def __init__(self,
+                 set_color_func: Callable[[tuple[float, ...]], None],
+                 grid_size: tuple[int, int] = (6, 5), parent=None):
+        super().__init__(parent)
+
+        self.setFrameStyle(QFrame.Shape.StyledPanel)
+        self.setLineWidth(3)
+        self.setContentsMargins(0, 0, 0, 0)
+        self._grid_size = grid_size
+        self._page_size = grid_size[0] * grid_size[1]
+        self._current_page = 0
+        self._set_color_func = set_color_func
+
+        self.swatches_layout = QGridLayout(self)
+        self.swatches_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignHCenter)
+        self.swatches_layout.setSpacing(0)
+
+        self._swatch_data: dict[tuple[float, ...], QPixmap] = {}
+        self._latent_values_by_index: list[tuple[float, ...]] = []
+
+        self._swatch_buttons: list[ColorPickerSwatch] = []
+
+        for i in range(grid_size[0] * grid_size[1]):
+            widget = ColorPickerSwatch()
+            widget.clicked.connect(lambda _, swatch_button_index=i: self.on_swatch_clicked(swatch_button_index))
+            self._swatch_buttons.append(widget)
+
+        # Add the page change buttons.
+        def change_page(increment: int):
+            num_values = len(self._latent_values_by_index)
+            page_count = num_values // self._page_size
+
+            if num_values % self._page_size > 0:
+                page_count += 1
+            if page_count < 1:
+                page_count = 1
+            max_page_index = page_count-1
+
+            new_page = self._current_page
+            new_page += increment
+            if new_page < 0:
+                new_page = 0
+            elif new_page > max_page_index:
+                new_page = max_page_index
+
+            if self._current_page != new_page:
+                self._current_page = new_page
+                self.update_palette()
+
+        left_button = QPushButton(parent=self)
+        left_button.setText("⬅️")
+        left_button.clicked.connect(lambda: change_page(-1))
+        left_button.setFixedWidth(40)
+        left_button.setFixedHeight(40)
+
+        right_button = QPushButton(parent=self)
+        right_button.setText("➡️")
+        right_button.clicked.connect(lambda: change_page(1))
+        right_button.setFixedWidth(40)
+        right_button.setFixedHeight(40)
+
+        self.swatches_layout.addWidget(
+            left_button,
+            grid_size[1], 0,
+            alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop,
+        )
+        self.swatches_layout.addWidget(
+            right_button,
+            grid_size[1], grid_size[0]-1,
+            alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop,
+        )
+
+    def _button_index_to_value_index(self, button_index: int) -> int:
+        return button_index + self._current_page * self._page_size
+
+    def _value_index_in_range(self, value_index: int) -> bool:
+        return 0 <= value_index < len(self._latent_values_by_index)
+
+    def on_color_used(self, value: tuple[float, ...], pixmap: QPixmap) -> None:
+        # Move or add the most recently used swatches at the front of the list.
+        if value in self._swatch_data:
+            self._latent_values_by_index.remove(value)
+            self._latent_values_by_index.insert(0, value)
+        else:
+            self._latent_values_by_index.insert(0, value)
+            self._swatch_data[value] = pixmap
+
+        self.update_palette()
+
+    def on_swatch_clicked(self, button_index: int):
+        value_index = self._button_index_to_value_index(button_index)
+        if not self._value_index_in_range(value_index):
+            return
+
+        self._set_color_func(self._latent_values_by_index[value_index])
+
+    def update_palette(self):
+        # Update the swatch preview images.
+        for i in range(self._page_size):
+            # Remove the widget. We may add it back in.
+            self._swatch_buttons[i].setParent(None)
+
+            value_index = self._button_index_to_value_index(i)
+            if not self._value_index_in_range(value_index):
+                continue
+
+            column = i % self._grid_size[0]
+            row = i // self._grid_size[0]
+            self.swatches_layout.addWidget(
+                self._swatch_buttons[i],
+                row, column,
+                alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop,
+            )
+            value = self._latent_values_by_index[value_index]
+            pixmap = self._swatch_data[value]
+            button = self._swatch_buttons[i]
+            button.set_image(pixmap)
+
+
 class ColorPickerWidget(QWidget):
     def __init__(self,
                  generate_preview_func: Callable[[tuple[float, float, float, float]], QPixmap],
@@ -80,8 +203,6 @@ class ColorPickerWidget(QWidget):
         super().__init__(parent)
 
         self._generate_preview_func = generate_preview_func
-        self._swatches_by_value: dict[tuple[float, ...], ColorPickerSwatch] = {}
-        self._swatches_by_index: list[ColorPickerSwatch] = []
 
         """
         Layout:
@@ -95,8 +216,11 @@ class ColorPickerWidget(QWidget):
         overall_layout = QVBoxLayout(self)
 
         slider_preview_layout = QHBoxLayout(self)
-        self.swatches_layout = QGridLayout(self)
-        self.swatches_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignLeft)
+        self.swatch_widget = ColorPickerSwatches(
+            set_color_func=self.set_current_latent_value,
+            grid_size=(6, 4),
+            parent=self
+        )
 
         self._color_preview = ColorPickerLatentPreview(self)
         sliders_layout = QVBoxLayout(self)
@@ -124,9 +248,9 @@ class ColorPickerWidget(QWidget):
         slider_preview_layout.setContentsMargins(0, 0, 0, 0)
 
         overall_layout.addLayout(slider_preview_layout)
-        overall_layout.addLayout(self.swatches_layout)
+        overall_layout.addWidget(self.swatch_widget)
         overall_layout.setSpacing(10)
-        overall_layout.setContentsMargins(0, 0, 0, 0)
+        # overall_layout.setContentsMargins(0, 0, 0, 0)
 
         self.setLayout(overall_layout)
 
@@ -154,9 +278,7 @@ class ColorPickerWidget(QWidget):
 
     def use_current_latent_value(self) -> tuple[float, ...]:
         latent_value = self._get_current_latent_value()
-
-        self.update_palette(latent_value)
-
+        self.swatch_widget.on_color_used(latent_value, self._color_preview.pixmap())
         return latent_value
 
     def set_current_latent_value(self, value: tuple[float, ...]) -> None:
@@ -164,34 +286,6 @@ class ColorPickerWidget(QWidget):
             self._sliders[i].value = value[i]
 
         self.update_preview(value)
-
-    def update_palette(self, value: tuple[float, ...]) -> None:
-        if value in self._swatches_by_value:
-            widget = self._swatches_by_value[value]
-            self._swatches_by_index.remove(widget)
-            self._swatches_by_index.append(widget)
-        else:
-            widget = ColorPickerSwatch()
-            widget.set_image(self._color_preview.pixmap())
-            widget.clicked.connect(lambda: self.set_current_latent_value(value))
-            self._swatches_by_value[value] = widget
-            self._swatches_by_index.append(widget)
-
-        # Remove all the widgets from the layout.
-        for widget in self._swatches_by_index:
-            self.swatches_layout.removeWidget(widget)
-
-        index = 0
-        num_columns = 6
-        for widget in reversed(self._swatches_by_index):
-            column = index % num_columns
-            row = index // num_columns
-            self.swatches_layout.addWidget(
-                widget,
-                row, column,
-                alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop,
-            )
-            index += 1
 
 
 class LatentPicker(QTabWidget):
@@ -202,5 +296,3 @@ class LatentPicker(QTabWidget):
 
         self.color_picker = ColorPickerWidget(generate_preview_func, parent=self)
         self.addTab(self.color_picker, "Solid Latent")
-
-
